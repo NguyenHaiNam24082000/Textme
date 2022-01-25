1c55ca02a25e2e0699274038c452f96a597dec23
nguyenhainam24082000 <nghainam2000@gmail.com>
2022-01-26 00:38:24 +0700

    update Workspace

--- client/src/pages/Workspace/index.js
+++ client/src/pages/Workspace/index.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  FullScreenDropzone,
+  IMAGE_MIME_TYPE,
+} from "@mantine/dropzone";
 
 export default function Workspace() {
-  return <div>hello</div>;
+  return (
+    <div className="flex w-full h-full">
+      <div className="w-2/4 h-full">aaaa</div>
+      <div className="w-2/4 h-full bg-black">
+      </div>
+      <FullScreenDropzone accept={IMAGE_MIME_TYPE}>
+          {() => {
+            return <div>jeejej</div>;
+          }}
+        </FullScreenDropzone>
+    </div>
+  );
 }
